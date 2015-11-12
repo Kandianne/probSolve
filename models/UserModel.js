7f50f07171d479f9954b54f1167fb123d6b66266
kandianne <kandipierre@gmail.com>
2015-11-11 17:58:48 -0800

    added userMOdel

---
+++ models/UserModel.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+var jwt = require("jsonwebtoken")
+
+var UserSchema = new mongoose.Schema({
+	googleId: String,
+	facebookId: String,
+	linkedInId: String,
+	displayName: String,
+	username: {type: String, lowercase: true, unique: true},
+	email: {type: String, lowercase: true, unique: true},
+	image: { type: String, default: 'http://1.bp.blogspot.com/-fDYO0D23HvM/VcdBvhO0FiI/AAAAAAAAAoU/7vi6V3TYHp4/s1600/Anonymous.png'},
+	passwordHash: String,
+	salt: String,
+	createdDate: Date,
+	deactivatedDate: Date,
+	problemsPosted: [{type: mongoose.Schema.Types.ObjectId, ref: "Problems"}],
+	solutionsPosted: [{type: mongoose.Schema.Types.ObjectId, ref: "Questions"}]
+});
+
+UserSchema.methods.generateJWT = function() {
+	var today = new Date();
+	var exp = new Date(today);
+	exp.setDate(today.getDate() + 36500);
+	return jwt.sign({
+		id : this._id,
+		username : this.username,
+		image: this.image,
+		exp: exp.getTime() / 1000		
+	}, "problem_solvers");
+}
+
+UserSchema.methods.setPassword = function(password) {
+	this.salt = crypto.randomBytes(64).toString('hex');
+	this.passwordHash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+}
+
+UserSchema.methods.checkPassword = function(password) {
+	var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+	return hash === this.passwordHash;
+};
+
+mongoose.model('User', UserSchema);
--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,12 +1,18 @@
 (function() {
 	'use strict';
-	angular.module('app', ['ui.router'])
+	angular.module('app', ['ui.router', 'ngMaterial'])
 	.config(Config);
 	Config.$inject = ['$stateProvider', '$urlRouterProvider'];
 	function Config($stateProvider, $urlRouterProvider) {
 		$stateProvider.state('Home',{
 			url: '/',
 			templateUrl: 'views/home.html'
+		}).state('Problems',{
+			url: '/problems',
+			templateUrl: 'views/problems.html'
+		}).state('Solutions',{
+			url: '/solutions',
+			templateUrl: 'views/solutions.html'
 		});
 		$urlRouterProvider.otherwise('/');
 	}
